7f2b2e67900b5214673f15b172114c01d9193600
CodexpathCommunity <codexpath3@gmail.com>
2022-03-06 15:09:05 +0100

    created user routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
 import { Express, Request, Response } from "express";
+import validateRequest from "./middleware/validateRequest";
+import { createUserHandler } from "./controller/user.controller";
+import { createUserSchema } from "./schema/user.schema";
 
 export default function (app: Express) {
   app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
   });
+
+  //register user
+  app.post("api/v1/user", validateRequest(createUserSchema), createUserHandler);
 }
