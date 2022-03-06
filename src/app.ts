cc203b01a9f68fb86d805007de53b7aff400be7d
CodexpathCommunity <codexpath3@gmail.com>
2022-03-06 12:00:13 +0100

    configured logger

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from "express";
+import config from "config";
+import log from "./logger";
+
+const port = config.get("port") as number;
+const host = config.get("host") as string;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, host, () => {
+  log.info("server started at http://%s:%s", host, port);
+});
